e2efbfb011b80962d60b0d2c1a208135c9ba93c1
kcarritt <kyrie.carritt@uleth.ca>
2016-03-02 08:11:48 -0700

    Added files via upload
    Final Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	
+	console.log("Good to go!")
 }); 
+
+$("#OSinfo").hide();
+
+$("#EEP").on('click', function() {
+	$("#EEPinfo").fadeIn("slow");
+	$("#OSinfo").hide();
+	$("#info").html("");
+	$("#EEP").css("color", "#b55a88");
+	$("#OS").css("color", "#720F37");
+	$("#EEP").css("margin-left", "6%");
+	$("#OS").css("margin-left", "5%");
+});
+
+$("#OS").on('click', function() {
+	$("#OSinfo").fadeIn("slow");
+	$("#EEPinfo").hide();
+	$("#OS").css("color", "#b55a88");
+	$("#EEP").css("color", "#720F37");
+	$("#OS").css("margin-left", "6%");
+	$("#EEP").css("margin-left", "5%");
+});
